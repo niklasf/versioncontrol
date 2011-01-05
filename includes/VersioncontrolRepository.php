5e676452eb4b940de7295a152f8091bbdc3c377a
Marco Villegas <marvil07@gmail.com>
2011-01-05 05:39:21 +0000

    task #723958: Remove ArrayAccess dependency everywhere.

--- includes/VersioncontrolRepository.php
+++ includes/VersioncontrolRepository.php
@@ -447,20 +447,6 @@ abstract class VersioncontrolRepository implements VersioncontrolEntityInterface
     return $this->pluginInstances['committer_mapper'];
   }
 
-
-  //ArrayAccess interface implementation FIXME soooooooo deprecated
-  public function offsetExists($offset) {
-    return isset($this->$offset);
-  }
-  public function offsetGet($offset) {
-    return $this->$offset;
-  }
-  public function offsetSet($offset, $value) {
-    $this->$offset = $value;
-  }
-  public function offsetUnset($offset) {
-    unset($this->$offset);
-  }
 }
 
 /**
