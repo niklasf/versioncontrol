415164c0b9b836a7776ca37dc940ca667c7ffe8a
Michael Prasuhn <mike@mikeyp.net>
2011-01-17 01:22:42 +0000

    #970200 by mikey_p: Update base commitlog_global_commits view

--- commitlog/includes/views/default_views/commitlog_repository_commits.view.php
+++ commitlog/includes/views/default_views/commitlog_repository_commits.view.php
@@ -2,8 +2,8 @@
 // $Id$
 
 $view = new view;
-$view->name = 'commitlog_repository_commits';
-$view->description = 'A per-repository commit log.';
+$view->name = 'commitlog_global_commits';
+$view->description = 'Commitlog Global Commit Log';
 $view->tag = 'VersionControl Core';
 $view->view_php = '';
 $view->base_table = 'versioncontrol_operations';
@@ -13,7 +13,7 @@ $view->disabled = FALSE; /* Edit this to true to make a default view disabled in
 $handler = $view->new_display('default', 'Defaults', 'default');
 $handler->override_option('fields', array(
   'attribution' => array(
-    'label' => 'Attribution',
+    'label' => '',
     'alter' => array(
       'alter_text' => 0,
       'text' => '',
@@ -76,6 +76,66 @@ $handler->override_option('fields', array(
     'field' => 'vc_op_id',
     'relationship' => 'none',
   ),
+  'date' => array(
+    'label' => 'Date',
+    'alter' => array(
+      'alter_text' => 0,
+      'text' => '',
+      'make_link' => 0,
+      'path' => '',
+      'link_class' => '',
+      'alt' => '',
+      'prefix' => '',
+      'suffix' => '',
+      'target' => '',
+      'help' => '',
+      'trim' => 0,
+      'max_length' => '',
+      'word_boundary' => 1,
+      'ellipsis' => 1,
+      'html' => 0,
+      'strip_tags' => 0,
+    ),
+    'empty' => '',
+    'hide_empty' => 0,
+    'empty_zero' => 0,
+    'date_format' => 'custom',
+    'custom_date_format' => 'F j, Y G:i',
+    'exclude' => 1,
+    'id' => 'date',
+    'table' => 'versioncontrol_operations',
+    'field' => 'date',
+    'relationship' => 'none',
+  ),
+  'name' => array(
+    'label' => '',
+    'alter' => array(
+      'alter_text' => 0,
+      'text' => '',
+      'make_link' => 0,
+      'path' => '',
+      'link_class' => '',
+      'alt' => '',
+      'prefix' => '',
+      'suffix' => '',
+      'target' => '',
+      'help' => '',
+      'trim' => 0,
+      'max_length' => '',
+      'word_boundary' => 1,
+      'ellipsis' => 1,
+      'html' => 0,
+      'strip_tags' => 0,
+    ),
+    'empty' => '',
+    'hide_empty' => 0,
+    'empty_zero' => 0,
+    'exclude' => 1,
+    'id' => 'name',
+    'table' => 'versioncontrol_labels',
+    'field' => 'name',
+    'relationship' => 'none',
+  ),
   'revision' => array(
     'label' => 'Revision',
     'alter' => array(
@@ -105,44 +165,17 @@ $handler->override_option('fields', array(
     'field' => 'revision',
     'relationship' => 'none',
   ),
-  'date' => array(
-    'label' => 'Date',
-    'alter' => array(
-      'alter_text' => 0,
-      'text' => '',
-      'make_link' => 0,
-      'path' => '',
-      'link_class' => '',
-      'alt' => '',
-      'prefix' => '',
-      'suffix' => '',
-      'target' => '',
-      'help' => '',
-      'trim' => 0,
-      'max_length' => '',
-      'word_boundary' => 1,
-      'ellipsis' => 1,
-      'html' => 0,
-      'strip_tags' => 0,
-    ),
-    'empty' => '',
-    'hide_empty' => 0,
-    'empty_zero' => 0,
-    'date_format' => 'small',
-    'custom_date_format' => 'G:i',
-    'exclude' => 1,
-    'id' => 'date',
-    'table' => 'versioncontrol_operations',
-    'field' => 'date',
-    'relationship' => 'none',
-  ),
   'nothing' => array(
     'label' => '',
     'alter' => array(
-      'text' => '<div class="commit-repository">Commit <strong>[revision]</strong> [attribution] at [date]</div>',
+      'text' => '<div class="commit-global">
+  <h3>[date]</h3>
+  <div class="commit-info">Commit <strong>[revision]</strong> on <strong>[name]</strong></div>
+  <div class="attribtution">[attribution]</div>
+</div>',
       'make_link' => 0,
-      'path' => '',
-      'link_class' => '',
+      'path' => '#',
+      'link_class' => 'global-author',
       'alt' => '',
       'prefix' => '',
       'suffix' => '',
@@ -188,7 +221,7 @@ $handler->override_option('fields', array(
     'hide_empty' => 0,
     'empty_zero' => 0,
     'view' => 'commitlog_commit_items',
-    'display' => 'default',
+    'display' => 'block_2',
     'arguments' => '[vc_op_id]',
     'query_aggregation' => 1,
     'exclude' => 0,
@@ -220,10 +253,39 @@ $handler->override_option('fields', array(
     'empty' => '',
     'hide_empty' => 0,
     'empty_zero' => 0,
-    'exclude' => 0,
+    'issue_tracker_url' => '',
+    'exclude' => 1,
     'id' => 'message',
     'table' => 'versioncontrol_operations',
     'field' => 'message',
+    'relationship' => 'none',
+  ),
+  'nothing_1' => array(
+    'label' => '',
+    'alter' => array(
+      'text' => '<pre>[message]</pre>',
+      'make_link' => 0,
+      'path' => '',
+      'link_class' => '',
+      'alt' => '',
+      'prefix' => '',
+      'suffix' => '',
+      'target' => '',
+      'help' => '',
+      'trim' => 0,
+      'max_length' => '',
+      'word_boundary' => 1,
+      'ellipsis' => 1,
+      'html' => 0,
+      'strip_tags' => 0,
+    ),
+    'empty' => '',
+    'hide_empty' => 0,
+    'empty_zero' => 0,
+    'exclude' => 0,
+    'id' => 'nothing_1',
+    'table' => 'views',
+    'field' => 'nothing',
     'relationship' => 'none',
   ),
 ));
@@ -237,52 +299,6 @@ $handler->override_option('sorts', array(
     'relationship' => 'none',
   ),
 ));
-$handler->override_option('arguments', array(
-  'repo_id' => array(
-    'default_action' => 'ignore',
-    'style_plugin' => 'default_summary',
-    'style_options' => array(),
-    'wildcard' => 'all',
-    'wildcard_substitution' => 'All',
-    'title' => '',
-    'breadcrumb' => '',
-    'default_argument_type' => 'fixed',
-    'default_argument' => '',
-    'validate_type' => 'none',
-    'validate_fail' => 'not found',
-    'break_phrase' => 0,
-    'not' => 0,
-    'id' => 'repo_id',
-    'table' => 'versioncontrol_repositories',
-    'field' => 'repo_id',
-    'validate_user_argument_type' => 'uid',
-    'validate_user_roles' => array(
-      '2' => 0,
-    ),
-    'relationship' => 'none',
-    'default_options_div_prefix' => '',
-    'default_argument_fixed' => '',
-    'default_argument_user' => 0,
-    'default_argument_mailfish_custom' => '',
-    'default_argument_php' => '',
-    'validate_argument_node_type' => array(
-      'project_project' => 0,
-      'page' => 0,
-      'story' => 0,
-    ),
-    'validate_argument_node_access' => 0,
-    'validate_argument_nid_type' => 'nid',
-    'validate_argument_vocabulary' => array(),
-    'validate_argument_type' => 'tid',
-    'validate_argument_transform' => 0,
-    'validate_user_restrict_roles' => 0,
-    'validate_argument_project_term_argument_type' => 'tid',
-    'validate_argument_project_term_argument_action_top_without' => 'pass',
-    'validate_argument_project_term_argument_action_top_with' => 'pass',
-    'validate_argument_project_term_argument_action_child' => 'pass',
-    'validate_argument_php' => '',
-  ),
-));
 $handler->override_option('access', array(
   'type' => 'none',
 ));
@@ -290,6 +306,6 @@ $handler->override_option('cache', array(
   'type' => 'none',
 ));
 $handler->override_option('use_pager', '1');
-
-// View 'commitlog_user_commits'
-
+$handler->override_option('style_options', array(
+  'grouping' => 'date_1',
+));
--- commitlog/includes/views/default_views/commitlog_user_commits.view.php
+++ commitlog/includes/views/default_views/commitlog_user_commits.view.php
@@ -2,8 +2,8 @@
 // $Id$
 
 $view = new view;
-$view->name = 'commitlog_user_commits';
-$view->description = 'Backend-agnostic per-user commit log';
+$view->name = 'commitlog_global_commits';
+$view->description = 'Commitlog Global Commit Log';
 $view->tag = 'VersionControl Core';
 $view->view_php = '';
 $view->base_table = 'versioncontrol_operations';
@@ -12,6 +12,35 @@ $view->api_version = 2;
 $view->disabled = FALSE; /* Edit this to true to make a default view disabled initially */
 $handler = $view->new_display('default', 'Defaults', 'default');
 $handler->override_option('fields', array(
+  'attribution' => array(
+    'label' => '',
+    'alter' => array(
+      'alter_text' => 0,
+      'text' => '',
+      'make_link' => 0,
+      'path' => '',
+      'link_class' => '',
+      'alt' => '',
+      'prefix' => '',
+      'suffix' => '',
+      'target' => '',
+      'help' => '',
+      'trim' => 0,
+      'max_length' => '',
+      'word_boundary' => 1,
+      'ellipsis' => 1,
+      'html' => 0,
+      'strip_tags' => 0,
+    ),
+    'empty' => '',
+    'hide_empty' => 0,
+    'empty_zero' => 0,
+    'exclude' => 1,
+    'id' => 'attribution',
+    'table' => 'versioncontrol_operations',
+    'field' => 'attribution',
+    'relationship' => 'none',
+  ),
   'vc_op_id' => array(
     'label' => 'Operation ID',
     'alter' => array(
@@ -70,12 +99,41 @@ $handler->override_option('fields', array(
     'empty' => '',
     'hide_empty' => 0,
     'empty_zero' => 0,
-    'date_format' => 'small',
-    'custom_date_format' => '2',
+    'date_format' => 'custom',
+    'custom_date_format' => 'F j, Y G:i',
     'exclude' => 1,
     'id' => 'date',
     'table' => 'versioncontrol_operations',
     'field' => 'date',
+    'relationship' => 'none',
+  ),
+  'name' => array(
+    'label' => '',
+    'alter' => array(
+      'alter_text' => 0,
+      'text' => '',
+      'make_link' => 0,
+      'path' => '',
+      'link_class' => '',
+      'alt' => '',
+      'prefix' => '',
+      'suffix' => '',
+      'target' => '',
+      'help' => '',
+      'trim' => 0,
+      'max_length' => '',
+      'word_boundary' => 1,
+      'ellipsis' => 1,
+      'html' => 0,
+      'strip_tags' => 0,
+    ),
+    'empty' => '',
+    'hide_empty' => 0,
+    'empty_zero' => 0,
+    'exclude' => 1,
+    'id' => 'name',
+    'table' => 'versioncontrol_labels',
+    'field' => 'name',
     'relationship' => 'none',
   ),
   'revision' => array(
@@ -110,7 +168,11 @@ $handler->override_option('fields', array(
   'nothing' => array(
     'label' => '',
     'alter' => array(
-      'text' => '<div class="commit-user">Commit <strong>[revision]</strong> [attribution] at [date]</div>',
+      'text' => '<div class="commit-global">
+  <h3>[date]</h3>
+  <div class="commit-info">Commit <strong>[revision]</strong> on <strong>[name]</strong></div>
+  <div class="attribtution">[attribution]</div>
+</div>',
       'make_link' => 0,
       'path' => '#',
       'link_class' => 'global-author',
@@ -191,10 +253,39 @@ $handler->override_option('fields', array(
     'empty' => '',
     'hide_empty' => 0,
     'empty_zero' => 0,
-    'exclude' => 0,
+    'issue_tracker_url' => '',
+    'exclude' => 1,
     'id' => 'message',
     'table' => 'versioncontrol_operations',
     'field' => 'message',
+    'relationship' => 'none',
+  ),
+  'nothing_1' => array(
+    'label' => '',
+    'alter' => array(
+      'text' => '<pre>[message]</pre>',
+      'make_link' => 0,
+      'path' => '',
+      'link_class' => '',
+      'alt' => '',
+      'prefix' => '',
+      'suffix' => '',
+      'target' => '',
+      'help' => '',
+      'trim' => 0,
+      'max_length' => '',
+      'word_boundary' => 1,
+      'ellipsis' => 1,
+      'html' => 0,
+      'strip_tags' => 0,
+    ),
+    'empty' => '',
+    'hide_empty' => 0,
+    'empty_zero' => 0,
+    'exclude' => 0,
+    'id' => 'nothing_1',
+    'table' => 'views',
+    'field' => 'nothing',
     'relationship' => 'none',
   ),
 ));
@@ -208,69 +299,6 @@ $handler->override_option('sorts', array(
     'relationship' => 'none',
   ),
 ));
-$handler->override_option('arguments', array(
-  'uid' => array(
-    'default_action' => 'not found',
-    'style_plugin' => 'default_summary',
-    'style_options' => array(),
-    'wildcard' => 'all',
-    'wildcard_substitution' => 'All',
-    'title' => '',
-    'breadcrumb' => '',
-    'default_argument_type' => 'fixed',
-    'default_argument' => '',
-    'validate_type' => 'user',
-    'validate_fail' => 'not found',
-    'break_phrase' => 0,
-    'not' => 0,
-    'id' => 'uid',
-    'table' => 'users',
-    'field' => 'uid',
-    'validate_user_argument_type' => 'uid',
-    'validate_user_roles' => array(
-      '2' => 0,
-      '3' => 0,
-      '4' => 0,
-      '5' => 0,
-      '6' => 0,
-      '7' => 0,
-      '8' => 0,
-    ),
-    'relationship' => 'none',
-    'default_options_div_prefix' => '',
-    'default_argument_fixed' => '',
-    'default_argument_user' => 0,
-    'default_argument_php' => '',
-    'validate_argument_node_type' => array(
-      'forum' => 0,
-      'project_project' => 0,
-      'project_release' => 0,
-      'project_issue' => 0,
-      'book' => 0,
-      'page' => 0,
-      'story' => 0,
-    ),
-    'validate_argument_node_access' => 0,
-    'validate_argument_nid_type' => 'nid',
-    'validate_argument_vocabulary' => array(
-      '1' => 0,
-      '4' => 0,
-      '2' => 0,
-      '3' => 0,
-    ),
-    'validate_argument_type' => 'tid',
-    'validate_argument_transform' => 0,
-    'validate_user_restrict_roles' => 0,
-    'validate_argument_project_term_vocabulary' => array(
-      '2' => 0,
-    ),
-    'validate_argument_project_term_argument_type' => 'tid',
-    'validate_argument_project_term_argument_action_top_without' => 'pass',
-    'validate_argument_project_term_argument_action_top_with' => 'pass',
-    'validate_argument_project_term_argument_action_child' => 'pass',
-    'validate_argument_php' => '',
-  ),
-));
 $handler->override_option('access', array(
   'type' => 'none',
 ));
@@ -281,3 +309,4 @@ $handler->override_option('use_pager', '1');
 $handler->override_option('style_options', array(
   'grouping' => 'date_1',
 ));
+
