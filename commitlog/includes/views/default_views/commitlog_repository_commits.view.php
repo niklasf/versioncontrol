4136a7504505607725c5fe93d6e7b96bababeb20
Marco Villegas <marvil07@gmail.com>
2011-01-28 21:16:59 +0000

    feature #1039042: Add a views field handler that retrieves all operation label names.
    This commit also modify commitlog_global_commits,
    commitlog_repository_commits, commitlog_individual_commit and
    commitlog_user_commits default view to use it instead of the current
    versioncontrol label name views fields, that naturally end up in one row
    per vc_op_id per label.

--- commitlog/includes/views/default_views/commitlog_repository_commits.view.php
+++ commitlog/includes/views/default_views/commitlog_repository_commits.view.php
@@ -108,7 +108,7 @@ $handler->override_option('fields', array(
     'field' => 'date',
     'relationship' => 'none',
   ),
-  'name' => array(
+  'labels' => array(
     'label' => '',
     'alter' => array(
       'alter_text' => 0,
@@ -132,9 +132,9 @@ $handler->override_option('fields', array(
     'hide_empty' => 0,
     'empty_zero' => 0,
     'exclude' => 1,
-    'id' => 'name',
-    'table' => 'versioncontrol_labels',
-    'field' => 'name',
+    'id' => 'labels',
+    'table' => 'versioncontrol_operations',
+    'field' => 'labels',
     'relationship' => 'none',
   ),
   'revision' => array(
@@ -171,7 +171,7 @@ $handler->override_option('fields', array(
     'alter' => array(
       'text' => '<div class="commit-global">
   <h3>[date]</h3>
-  <div class="commit-info">Commit <strong>[revision]</strong> on <strong>[name]</strong></div>
+  <div class="commit-info">Commit <strong>[revision]</strong> on <strong>[labels]</strong></div>
   <div class="attribtution">[attribution]</div>
 </div>',
       'make_link' => 0,
